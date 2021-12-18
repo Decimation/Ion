e90dc0d2b7c5dac77d317da3b1bb2b79801e1884
Read <decimation001@gmail.com>
2021-12-18 02:49:33 -0600

    Add project files.

---
+++ Ion/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics;
+using Kantan.Text;
+using Novus.OS;
+using Novus.OS.Win32.Wrappers;
+
+namespace Ion;
+
+public static class Program
+{
+	public static void Main(string[] args)
+	{
+
+#if DEBUG
+		args = new[]
+		{
+			"symbols",
+			@"C:\Symbols\charmap.pdb",
+
+			"GetUName"
+			// "*"
+		};
+
+#endif
+
+		Debug.WriteLine($"{args.QuickJoin()}");
+
+		if (!args.Any()) {
+			Help();
+			return;
+		}
+
+
+		var op = args.First();
+		var sp = new Span<string>(args);
+
+		switch (op) {
+			case "symbols":
+				if (args.Length < 2) {
+					break;
+				}
+
+				Run_Symbols(sp[1..]);
+				break;
+			default:
+				break;
+		}
+
+	
+
+
+	private static void Run_Symbols(Span<string> args)
+	{
+		const string ALL = "*";
+
+		var img = args[0];
+		var id  = args.Length < 2 ? ALL : args[1];
+
+		Debug.WriteLine($"{nameof(Run_Symbols)}: {img} {id}");
+
+		using var r = new SymbolReader(img);
+
+		if (id == ALL) {
+			r.LoadAll();
+
+			foreach (Symbol symbol in r.SymbolsCache) {
+				Console.WriteLine(symbol);
+			}
+
+		}
+		else {
+			var s = r.GetSymbol(id);
+			Console.WriteLine(s);
+		}
+	}
+
+	private static void Help() { }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Ion;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+	public class Tests
+	{
+		[SetUp]
+		public void Setup() { }
+
+		
+		[Test]
+		public void Test1()
+		{
+			
+
+		}
+	}
+}
